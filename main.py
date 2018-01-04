b5674db0cf10621253483d4663cbc85b9facc99b
Nik Kovacic <nik.kovacic10@gmail.com>
2018-01-04 19:30:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+import os
+import jinja2
+import webapp2
+
+
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
+
+
+class BaseHandler(webapp2.RequestHandler):
+
+    def write(self, *a, **kw):
+        return self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        return self.write(self.render_str(template, **kw))
+
+    def render_template(self, view_filename, params=None):
+        if params is None:
+            params = {}
+        template = jinja_env.get_template(view_filename)
+        return self.response.out.write(template.render(params))
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        return self.render_template("hello.html")
+
+
+class OMeniHandler(BaseHandler):
+    def get(self):
+        return self.render_template("o_meni.html")
+
+
+class MojiProjektiHandler(BaseHandler):
+    def get(self):
+        return self.render_template("moji_projekti.html")
+
+
+class BlogHandler(BaseHandler):
+    def get(self):
+        return self.render_template("blog.html")
+
+
+class KontaktHandler(BaseHandler):
+    def get(self):
+        return self.render_template("kontakt.html")
+
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler),
+    webapp2.Route('/o_meni', OMeniHandler),
+    webapp2.Route('/moji_projekti', MojiProjektiHandler),
+    webapp2.Route('/blog', BlogHandler),
+    webapp2.Route('/kontakt', KontaktHandler),
+], debug=True)
